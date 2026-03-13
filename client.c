13a91c9fca6c00b4720af9b7bfe726b46228b659
Max <m1doroshin@gmail.com>
2026-03-13 18:54:14 +0300

    first commit

---
+++ client.c
@@ -0,0 +1,166 @@
+#define BS_SERVER_IP "127.0.0.1"
+#define BS_SERVER_PORT 443
+
+#define BS_CONN_LENGTH 10
+
+
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pty.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <stdbool.h>
+#define STB_DS_IMPLEMENTATION
+#include "stb_ds.h"
+#include <pthread.h>
+
+int serial_fd;
+int bs_fd;
+struct sockaddr_in bs_server_addr;
+
+int sent_bytes = 0;
+bool connected = false;
+
+void sleep_ms(int ms){
+    struct timespec req;
+    req.tv_sec = 0;
+    req.tv_nsec = ms * 1000000;
+    nanosleep(&req, NULL); 
+}
+
+void bs_reconnect(){
+    printf("reconnecting...\n");
+
+    if ((bs_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Ошибка создания сокета");
+        exit(EXIT_FAILURE);
+    }
+    while(!connected){
+        if (connect(bs_fd, (struct sockaddr *)&bs_server_addr, sizeof(bs_server_addr)) < 0) {
+            perror("Ошибка подключения");
+            continue;
+        }
+        connected = true;
+    }
+
+    sent_bytes = 0;
+
+    printf("reconnected\n");
+}
+
+void* serial_to_bs_loop(void* args){
+    printf("serial input loop started\n");
+
+    uint8_t buf[65535];
+    while(true){
+        while(!connected){
+            sleep_ms(1);
+        }
+        int len = read(serial_fd, buf, BS_CONN_LENGTH-sent_bytes);
+        sent_bytes += len;
+        if (len > 0) {
+            write(bs_fd, buf, len);
+        }else{
+            perror("ошибка одна и ошибся ты");
+        }
+        if(sent_bytes == BS_CONN_LENGTH){
+            printf("reached sent bytes limit\n");
+            connected = false;
+            shutdown(bs_fd, SHUT_RDWR);
+        }
+    }
+
+    return NULL;
+}
+
+void* bs_to_serial_loop(void* args){
+    printf("serial output loop started\n");
+
+    while(true){
+        while(!connected){
+            sleep_ms(1);
+        }
+        uint8_t buf[65535];
+        int len = read(bs_fd, buf, sizeof(buf));
+        if(len>0){
+            write(serial_fd, buf, len);
+        }else{
+            perror("aboba");
+            printf("received EOF\n");
+            connected = false;
+            close(bs_fd);
+            bs_reconnect();
+        }
+    }
+
+    return NULL;
+}
+
+int main() {
+    //create serial port
+    int serial_slave_fd;
+    char serial_name[256];
+    if (openpty(&serial_fd, &serial_slave_fd, serial_name, NULL, NULL) == -1) {
+        perror("openpty failed");
+        exit(1);
+    }
+    printf("created serial port: %s\n", serial_name);
+
+    //create BS
+    memset(&bs_server_addr, '0', sizeof(bs_server_addr));
+
+    bs_server_addr.sin_family = AF_INET;
+    bs_server_addr.sin_port = htons(BS_SERVER_PORT);
+
+    if(inet_pton(AF_INET, BS_SERVER_IP, &bs_server_addr.sin_addr) <= 0) {
+        perror("Неверный IP адрес");
+        exit(EXIT_FAILURE);
+    }
+
+    if ((bs_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Ошибка создания сокета");
+        exit(EXIT_FAILURE);
+    }
+
+    if (connect(bs_fd, (struct sockaddr *)&bs_server_addr, sizeof(bs_server_addr)) < 0) {
+        perror("Ошибка подключения");
+        exit(EXIT_FAILURE);
+    }
+
+    connected = true;
+
+    //create threads
+    pthread_t serial_output_thread;
+    if (pthread_create(&serial_output_thread, NULL, bs_to_serial_loop, NULL) != 0) {
+        perror("Error creating serial_output_thread");
+    }
+    pthread_t serial_input_thread;
+    if (pthread_create(&serial_input_thread, NULL, serial_to_bs_loop, NULL) != 0) {
+        perror("Error creating serial_input_thread");
+    }
+
+    sleep_ms(100);
+
+    printf("commands:\n");
+    printf(" - exit\n");
+
+    while(true){
+        char line[128];
+        printf("> ");
+        fflush(stdout);
+        if (fgets(line, 128, stdin) != NULL) {
+            line[strcspn(line, "\n")] = 0;
+            if(strcmp("exit",line)==0){
+                break;
+            }
+        } else {
+            perror("Error reading console input");
+        }
+    }
+
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,182 @@
+#define BS_SERVER_PORT 443
+
+#define BS_CONN_LENGTH 10
+
+
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pty.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <stdbool.h>
+#define STB_DS_IMPLEMENTATION
+#include "stb_ds.h"
+#include <pthread.h>
+
+int serial_fd;
+int bs_fd;
+int server_fd;
+struct sockaddr_in address;
+int addrlen = sizeof(address);
+
+int sent_bytes = 0;
+bool connected = false;
+
+void sleep_ms(int ms){
+    struct timespec req;
+    req.tv_sec = 0;
+    req.tv_nsec = ms * 1000000;
+    nanosleep(&req, NULL); 
+}
+
+void bs_reconnect(){
+    printf("reconnecting...\n");
+
+    if ((bs_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Ошибка создания сокета");
+        exit(EXIT_FAILURE);
+    }
+    while(!connected){
+        if ((bs_fd = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+            perror("Ошибка подключения");
+            continue;
+        }
+        connected = true;
+    }
+
+    sent_bytes = 0;
+
+    printf("reconnected\n");
+}
+
+void* serial_to_bs_loop(void* args){
+    printf("serial input loop started\n");
+
+    uint8_t buf[65535];
+    while(true){
+        while(!connected){
+            sleep_ms(1);
+        }
+        int len = read(serial_fd, buf, BS_CONN_LENGTH-sent_bytes);
+        sent_bytes += len;
+        if (len > 0) {
+            write(bs_fd, buf, len);
+        }else{
+            perror("ошибка одна и ошибся ты");
+        }
+        if(sent_bytes == BS_CONN_LENGTH){
+            printf("reached sent bytes limit\n");
+            connected = false;
+            shutdown(bs_fd, SHUT_RDWR);
+        }
+    }
+
+    return NULL;
+}
+
+void* bs_to_serial_loop(void* args){
+    printf("serial output loop started\n");
+
+    while(true){
+        while(!connected){
+            sleep_ms(1);
+        }
+        uint8_t buf[65535];
+        int len = read(bs_fd, buf, sizeof(buf));
+        if(len>0){
+            write(serial_fd, buf, len);
+        }else{
+            perror("aboba");
+            printf("received EOF\n");
+            connected = false;
+            close(bs_fd);
+            bs_reconnect();
+        }
+    }
+
+    return NULL;
+}
+
+int main() {
+    //create serial port
+    int serial_slave_fd;
+    char serial_name[256];
+    if (openpty(&serial_fd, &serial_slave_fd, serial_name, NULL, NULL) == -1) {
+        perror("openpty failed");
+        exit(1);
+    }
+    printf("created serial port: %s\n", serial_name);
+
+    //create BS
+    // Создаем сокет
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("Ошибка создания сокета");
+        exit(EXIT_FAILURE);
+    }
+
+    // Разрешаем повторное использование порта
+    int opt = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+        perror("Ошибка setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    // Заполняем структуру адреса
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(BS_SERVER_PORT);
+
+    // Привязываем сокет к адресу и порту
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("Ошибка привязки");
+        exit(EXIT_FAILURE);
+    }
+
+    // Начинаем слушать входящие подключения
+    if (listen(server_fd, 3) < 0) {
+        perror("Ошибка listen");
+        exit(EXIT_FAILURE);
+    }
+
+    if ((bs_fd = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+        perror("Ошибка accept");
+        exit(EXIT_FAILURE);
+    }
+
+    connected = true;
+
+    //create threads
+    pthread_t serial_output_thread;
+    if (pthread_create(&serial_output_thread, NULL, bs_to_serial_loop, NULL) != 0) {
+        perror("Error creating serial_output_thread");
+    }
+    pthread_t serial_input_thread;
+    if (pthread_create(&serial_input_thread, NULL, serial_to_bs_loop, NULL) != 0) {
+        perror("Error creating serial_input_thread");
+    }
+
+    sleep_ms(100);
+
+    printf("commands:\n");
+    printf(" - exit\n");
+
+    while(true){
+        char line[128];
+        printf("> ");
+        fflush(stdout);
+        if (fgets(line, 128, stdin) != NULL) {
+            line[strcspn(line, "\n")] = 0;
+            if(strcmp("exit",line)==0){
+                break;
+            }
+        } else {
+            perror("Error reading console input");
+        }
+    }
+
+    return 0;
+}
